241e80309b45fec6af2cd4d8a49982e1e1337613
Milos Popovic <milos_popovic@outlook.com>
2020-07-23 06:19:07 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,56 @@
+require 'pry'
 class Song
+
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    #binding.pry
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+  def self.find_or_create_by_name(name)
+
+      self.find_by_name(name) || self.create_by_name(name) #pitaj za ovo
+  end
+  def self.alphabetical
+    self.all.sort_by{|song| song.name} #pitaj sto ne radi sort
+  end
+
+  def self.new_from_filename(file)
+    data = file.split(" - ")
+    artist_name = data[0]
+    song_name = data[1]
+    song = self.new
+    song.name = song_name.gsub(".mp3", "")
+    song.artist_name = artist_name
+    song
+  end
+  def self.create_from_filename(file)
+    song = self.new_from_filename(file)
+    song.save
+    song
+  end
+  def self.destroy_all
+    self.all.clear
+  end
   def self.all
     @@all
   end
