46743d0b90be8a52ea174b5b76f45f93a391e249
Milos Popovic <milos_popovic@outlook.com>
2020-07-23 05:35:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,13 @@ class Song
   def self.find_by_name(name)
     self.all.find{|song| song.name == name}
   end
-
+  def self.find_or_create_by_name
+    if self.all.find{|song| song.name == name}
+      song
+    else
+      self.create_by_name(name)
+    end
+  end
   def self.all
     @@all
   end
