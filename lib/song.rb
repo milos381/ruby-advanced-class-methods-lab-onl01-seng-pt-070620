e312257ece29c3f1b7828e329c174085b051b1bb
Milos Popovic <milos_popovic@outlook.com>
2020-07-23 06:12:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,16 +35,18 @@ class Song
   end
 
   def self.new_from_filename(file)
-      data = file.split(" - ")
-      artist_name = data[0]
-      song_name = data[1]
-      song = self.new
-      song.name = song_name.gsub(".mp3", "")
-      song.artist_name = artist_name
-      song
+    data = file.split(" - ")
+    artist_name = data[0]
+    song_name = data[1]
+    song = self.new
+    song.name = song_name.replace(".mp3")
+    song.artist_name = artist_name
+    song
   end
   def self.create_from_filename
-
+    song = self.create
+    song.name = name
+    song
   end
   def self.destroy_all
     self.all.clear
